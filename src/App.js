f417dafe8544d51061351efc2e1c5880b029ed2e
Yuu Hikida <ponponda0103@gmail.com>
2024-06-18 09:58:56 +0900

    処理判定,

--- src/App.js
+++ src/App.js
@@ -16,16 +16,39 @@ function Square({value,onSquareClick}){
   );
 }
 export default function Board() {
+  const [xIsNext,setXIsNext]   = useState(true); 
   const [squares,setSequares] = useState(Array(9).fill(null));//nullの配列が9個
   
   function handleClick(i){
+    if(squares[i] || calculateWinner(squares)){
+        //値が入っている時のみreturn実行 
+        return ;
+      }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if(xIsNext)
+      {
+        nextSquares[i]="X";
+      }
+      else
+      {
+        nextSquares[i] = "O";
+      }
     setSequares(nextSquares);
+    setXIsNext(!xIsNext); 
+  }
+
+    //勝利判定
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
   
   return (
     <>
+    <div className="status">{status}</div>
     <div className="board-row">
       <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
       <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
@@ -46,5 +69,25 @@ export default function Board() {
   
   </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
